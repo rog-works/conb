0c2091e7829ab640716d0cd9e8167ebae329c589
rog-works <rog_works@yahoo.co.jp>
2017-03-01 09:44:17 +0900

    enum to string literal. update css

--- nodered/app/src/components/Component.ts
+++ nodered/app/src/components/Component.ts
@@ -1,9 +1,10 @@
 import * as ko from 'knockout-es5';
+import StringUtil from '../utils/StringUtil';
 
 export default class Component {
 	public static regist(construct: any, template: string): void {
 		ko.components.register(
-			construct.name,
+			StringUtil.snakelize(construct.name),
 			{
 				viewModel: {
 					createViewModel: (params: any, componentInfo: KnockoutComponentTypes.ComponentInfo): any => {
--- nodered/app/src/components/Panel.ts
+++ nodered/app/src/components/Panel.ts
@@ -2,72 +2,43 @@ import * as ko from 'knockout-es5';
 import Util from '../utils/Util';
 import Component from './Component';
 
-export enum Anchors {
-	None,
-	Left,
-	Right,
-	Top,
-	Bottom
+type Anchors = 'none' | 'left' | 'right' | 'top' | 'bottom';
+namespace Anchors {
+	export const None = 'none'
+	export const Left = 'left'
+	export const Right = 'right'
+	export const Top = 'top'
+	export const Bottom = 'bottom'
 }
 
-export enum Aligns {
-	None,
-	Center
+type Aligns = 'none' | 'center';
+namespace Aligns {
+	export const None = 'none'
+	export const Center = 'center'
 }
 
-export enum Positions {
-	None,
-	Absolute,
-	Relative,
-	Fixed
+type Positions = 'none' | 'absolute' | 'relative' | 'fixed';
+namespace Positions {
+	export const None = 'none'
+	export const Absolute = 'absolute'
+	export const Relative = 'relative'
+	export const Fixed = 'fixed'
 }
 
-export enum Docks {
-	None,
-	Fill
+type Docks = 'none' | 'fill';
+namespace Docks {
+	export const None = 'none'
+	export const Fill = 'fill'
 }
 
 export interface PanelEntity {
-	anchor?: string
-	align?: string
-	position?: string
-	dock?: string
+	anchor?: Anchors
+	align?: Aligns
+	position?: Positions
+	dock?: Docks
 	w?: number
 	h?: number
 	children?: any[]
-}
-
-const toAnchor = (str: string): Anchors => {
-	switch(str) {
-	case Anchors[Anchors.Left]: return Anchors.Left;
-	case Anchors[Anchors.Right]: return Anchors.Right;
-	case Anchors[Anchors.Top]: return Anchors.Top;
-	case Anchors[Anchors.Bottom]: return Anchors.Bottom;
-	default: return Anchors.None;
-	}
-}
-
-const toAlign = (str: string): Aligns => {
-	switch(str) {
-	case Aligns[Aligns.Center]: return Aligns.Center;
-	default: return Aligns.None;
-	}
-}
-
-const toPosition = (str: string): Positions => {
-	switch(str) {
-	case Positions[Positions.Absolute]: return Positions.Absolute;
-	case Positions[Positions.Relative]: return Positions.Relative;
-	case Positions[Positions.Fixed]: return Positions.Fixed;
-	default: return Positions.None;
-	}
-}
-
-const toDock = (str: string): Docks => {
-	switch(str) {
-	case Docks[Docks.Fill]: return Docks.Fill;
-	default: return Docks.None;
-	}
 }
 
 export default class Panel {
@@ -78,12 +49,12 @@ export default class Panel {
 	private _w: number
 	private _h: number
 	public children: any[]
-	public css: KnockoutComputed<any>
+	public css: KnockoutComputed<string>
 	public constructor(entity: PanelEntity) {
-		this._anchor = toAnchor(entity.anchor || Anchors[Anchors.None]),
-		this._align = toAlign(entity.align || Aligns[Aligns.None]),
-		this._position = toPosition(entity.position || Positions[Positions.None]),
-		this._dock = toDock(entity.dock || Docks[Docks.None]),
+		this._anchor = entity.anchor || Anchors.None,
+		this._align = entity.align || Aligns.None,
+		this._position = entity.position || Positions.None,
+		this._dock = entity.dock || Docks.None,
 		this._w = entity.w || 1,
 		this._h = entity.h || 1,
 		this.children = entity.children || [];
@@ -132,48 +103,48 @@ export default class Panel {
 	public set h(h: number) {
 		this._h = h;
 	}
-	private _computedCss(): any {
-		return Util.extend(
-			{ [Panel._anchorToCss(this.anchor)]: true },
-			{ [Panel._alignToCss(this.align)]: true },
-			{ [Panel._positionToCss(this.position)]: true },
-			{ [Panel._dockToCss(this.dock)]: true },
-			{ [Panel._wToCss(this.w)]: true },
-			{ [Panel._hToCss(this.h)]: true }
-		);
+	private _computedCss(): string {
+		return [
+			Panel._anchorToCss(this.anchor),
+			Panel._alignToCss(this.align),
+			Panel._positionToCss(this.position),
+			Panel._dockToCss(this.dock),
+			Panel._wToCss(this.w),
+			Panel._hToCss(this.h)
+		].join(' ');
 	}
-	private static _anchorToCss(anchor: Anchors): any {
+	private static _anchorToCss(anchor: Anchors): string {
 		switch(anchor) {
 		case Anchors.Left: return require('./assets/Panel.css').anchorLeft;
 		case Anchors.Bottom: return require('./assets/Panel.css').anchorBotton;
 		default: return require('./assets/Panel.css').alignNone;
 		}
 	}
-	private static _alignToCss(align: Aligns): any {
+	private static _alignToCss(align: Aligns): string {
 		switch(align) {
 		case Aligns.Center: return require('./assets/Panel.css').alignCenter;
 		default: return require('./assets/Panel.css').alignNone;
 		}
 	}
-	private static _positionToCss(position: Positions): any {
+	private static _positionToCss(position: Positions): string {
 		switch(position) {
 		case Positions.Absolute: return require('./assets/Panel.css').positionAbsolute;
 		default: return require('./assets/Panel.css').positionNone;
 		}
 	}
-	private static _dockToCss(dock: Docks): any {
+	private static _dockToCss(dock: Docks): string {
 		switch(dock) {
 		case Docks.Fill: return require('./assets/Panel.css').dockFill;
 		default: return require('./assets/Panel.css').dockNone;
 		}
 	}
-	private static _wToCss(w: number): any {
+	private static _wToCss(w: number): string {
 		switch(w) {
 		case 8: return require('./assets/Panel.css').w8;
 		}
 		throw new Error(`Not supported width. ${w}`);
 	}
-	private static _hToCss(h: number): any {
+	private static _hToCss(h: number): string {
 		switch(h) {
 		case 10: return require('./assets/Panel.css').h10;
 		}
